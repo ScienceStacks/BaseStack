ab6c9cdda579d4f6e92614d170f1d003d38d7da9
Joseph Hellerstein <jlheller@uw.edu>
2015-06-08 18:17:42 -0700

    Create a starting file for apache

--- install_scripts/apache_install.sh
+++ install_scripts/apache_install.sh
@@ -5,6 +5,8 @@ sudo apt-get install -y apache2
 # Handle the case of running without vagrant
 if ! [ -L /vagrant ]; then
   mkdir $HOME/apache_home
+  mkdir $HOME/apache_home/html
+  echo "Hello ubuntu World!" > $HOME/apache_home/html/index.html
   sudo ln -s $HOME/apache_home /vagrant
 fi
 
