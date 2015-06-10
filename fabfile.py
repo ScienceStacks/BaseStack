e20b685ab3c34349a270285b2fda21190744d2eb
Joseph Hellerstein <jlheller@uw.edu>
2015-06-10 16:33:21 -0700

    Added debugging flags in fabric_solo, fixed more bugs

--- fabfile.py
+++ fabfile.py
@@ -1,27 +1,24 @@
-from fabric.api import sudo
-from fabric.contrib.files import exists
 from fabric.context_managers import cd
-from fabric_solo import apt_get, chmod, cp, mkdir, mv, rm, runall, wget
+from fabric_solo import apt_get, chmod, cp, exists, ln
+from fabric_solo import mkdir, mv, rm, runall, wget
 
 def setup(git_email="jlheller@uw.edu", git_username="Joseph Hellerstein"):
   # Invokes all setups to do
-  print "git_email=%s git_username=%s" % (git_email, git_username)
-  return
   install_tools()
   setup_env(git_email, git_username)
   setup_apache()
 
 def setup_apache():
-  sudo("apt-get install -y apache2", pty=True)
+  apt_get("-y apache2", isSudo=True)
   if not exists("/vagrant"):
     mkdir("$HOME/apache_home", isSudo=False)
     mkdir("$HOME/apache_home/html", isSudo=False)
     command = 'echo "Hello ubuntu World!" > '
     command += '$HOME/apache_home/html/index.html'
     runall([command], isSudo=False)
-    ln("$HOME/apache_home", "/vagrant", "-s", isSudo=False)
-  sudo("rm -rf /var/www", pty=True)
-  sudo ("ln -fs /vagrant /var/www")
+    ln("s", "$HOME/apache_home", "/vagrant", isSudo=True)
+  runall(["rm -rf /var/www"], isSudo=True)
+  runall(["ln -fs /vagrant /var/www"], isSudo=True)
 
 def setup_env(git_email, git_username):
   cp('$HOME/BaseStack/bin/.bashrc', '$HOME', isSudo=False)
@@ -45,17 +42,17 @@ def install_chef():
        chown -R ubuntu ubuntu chef-repo
        rm master
     '''
-    runall(commands.split('\n'), sudo=True)
+    runall(commands.split('\n'), IsSudo=True)
     if exists('BaseStack/cookbooks'):
       command = 'cp -r BaseStack/cookbooks/* chef-repo/cookbooks/'
       runall([command], isSudo=False)
 
 def install_tools():
-  apt_get("python-pip python-dev build-essential")
+  apt_get("python-pip python-dev build-essential", isSudo=True)
   commands = '''
     pip install --upgrade pip 
     pip install --upgrade virtualenv 
   '''
   runall(commands.split('\n'), isSudo=True)
-  apt_get(curl)
-  apt_get(vim)
+  apt_get("curl", isSudo=True)
+  apt_get("vim", isSudo=True)
---
+++ fabric_solo.py
@@ -0,0 +1,86 @@
+# Functions used when using fabric on a single machine
+
+from fabric.api import env, local, sudo
+import fabric.contrib.files as fcf
+from fabric.context_managers import cd
+
+env.user = 'ubuntu'
+env.hosts = ['localhost']
+
+DEBUG = False # Default value is true
+DEBUG_EXISTS_OUTPUT = False  # Value returned by exists
+
+def runall(commands, isSudo=False):
+  # Run sudo on a list of commands
+  # Input: commands - list of commands
+  #        isSudo - command should be run using sudo
+  for cmd in commands:
+    if cmd.strip():  # Must be a string with non-blanks
+      if isSudo:
+        try:
+          if not DEBUG:
+            sudo(cmd, pty=True)
+          else:
+            print "[sudo debug] %s" % cmd
+        except Exception as e:
+          print e
+      else:
+        try: 
+          if not DEBUG:
+            print "[local] %s" % cmd
+            local(cmd, env.user)
+          else:
+            print "[local debug] %s" % cmd
+        except Exception as e:
+          print e
+
+def cp(from_path, to_path, isSudo=False):
+  # Copies one file path to another
+  # TBD: Handle errors
+  command = "cp %s %s" % (from_path, to_path)
+  runall([command], isSudo=True)
+  if not isSudo:
+    runall([command])
+
+def chown(path, new_owner):
+  # Changes the owner for the file path
+  command = 'chown -R %s:%s %s' % (new_owner, new_owner, path)
+  runall([command], isSudo=True)
+
+def chmod(mode, path, isSudo=False):
+  command = "chmod %s %s" % (mode, path)
+  runall([command], isSudo=isSudo)
+
+def ln(options, target, link, isSudo=False):
+  if not exists(link):
+    command = "ln -%s %s %s" % (options, target, link)
+    runall([command], isSudo=isSudo)
+
+def mkdir(path, isSudo=False):
+  if not exists(path):
+    command = "mkdir %s" % path
+    runall([command], isSudo=isSudo)
+
+def rm(path, isSudo=False):
+  if exists(path):
+    command = "rm %s" % path
+    runall([command], isSudo=isSudo)
+
+def mv(from_path, to_path, isSudo=False):
+  if exists(from_path):
+    command = "mv %s %s" % (from_path, to_path)
+    runall([command], isSudo=isSudo)
+
+def apt_get(args, isSudo=False):
+  command = "apt-get install %s" % args
+  runall([command], isSudo=isSudo)
+
+def wget(url):
+  command = "wget '{}'".format(url)
+  runall([command], isSudo=isSudo)
+
+def exists(path):
+  if not DEBUG:
+    return fcf.exists(path)
+  else:
+    return DEBUG_EXISTS_OUTPUT
