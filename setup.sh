0d4b78549fe1470583acf9e87f9d182966a3542c
Joseph Hellerstein <UWSoftwareEngineering@users.noreply.github.com>
2015-06-14 18:11:21 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Assumes that git is installed and have cloned the BaseStack repo
 # Install fabric
+sudo apt-get build-dep fabric
 sudo apt-get install fabric
+sudo apt-get build-dep ssh
 sudo apt-get install ssh
 cd $HOME/BaseStack
 fab setup
