55ff7ec48020a79a52e042bedae9ed2e732b0528
Joseph Hellerstein <jlheller@uw.edu>
2015-06-17 16:04:02 -0700

    Suppressing apt-get output

--- setup.sh
+++ setup.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 # Assumes that git is installed and have cloned the BaseStack repo
 # Install fabric
-sudo apt-get build-dep fabric
-sudo apt-get install fabric
-sudo apt-get build-dep ssh
-sudo apt-get install ssh
-cd $HOME/BaseStack
-fab setup
+FILE_OUT="/tmp/base_stack.txt"
+
+function apt_get {
+# Arg 1: package
+  echo "***apt-get build-dep $1" &>> $FILE_OUT
+  sudo apt-get build-dep $1 &>> $FILE_OUT
+  echo "***apt-get install $1" &>> $FILE_OUT
+  sudo apt-get install $1 & $FILE_OUT
+}
+
+# Do the installs
+apt_get fabric
+apt_get ssh
+
+cd $HOME/BaseStack/fabric_files
+#fab setup
--- setup_vm.sh
+++ setup_vm.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
+FILE_OUT="/tmp/base_stack.txt"
+
+function apt_get {
+# Arg 1: package
+  echo "apt-get build-dep $1" >> $FILE_OUT
+  sudo apt-get build-dep $1 &>> $FILE_OUT
+  echo "apt-get install $1" >> $FILE_OUT
+  sudo apt-get install $1 &>> $FILE_OUT
+}
+
+# Update the local repository
+sudo apt-get update > $FILE_OUT
 
 # Install git
-sudo apt-get build-dep git
-sudo apt-get install git
+apt_get git
 
 # Get the base repo
 git clone https://github.com/ScienceStacks/BaseStack.git
