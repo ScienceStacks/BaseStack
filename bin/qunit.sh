45f99e03c99b2ef57fb5663b0bd6525abf0f578e
Joseph Hellerstein <jlheller@uw.edu>
2015-09-11 15:45:24 -0700

    Added default argument for qunit.sh

--- bin/qunit.sh
+++ bin/qunit.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # Command line test for qunit. Assumes installation in npm
-node-qunit-phantomjs $1
+if [ $# -eq 0 ]; then
+  FFILE="tests.html"
+else
+  FFILE=$1
+fi
+echo $FFILE
+node-qunit-phantomjs $FFILE
