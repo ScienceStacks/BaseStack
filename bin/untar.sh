058cc3182c4722042848a2446f2c6a96d26cf870
Joseph Hellerstein <jlheller@uw.edu>
2015-09-14 17:34:36 -0700

    Generalized untar

--- bin/untar.sh
+++ bin/untar.sh
@@ -1,2 +1,10 @@
-!# /bin/bash
-tar -xvzf $1
+#! /bin/bash
+# Handles tar.zip and tar.bz2 files
+if ( echo $1 | grep -q ".tar.zip$" )
+then
+  tar -xvzf $1
+fi
+if ( echo $1 | grep -q ".tar.bz2$" )
+then
+  tar -vxjf $1
+fi
