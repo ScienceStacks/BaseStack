9d5b70c490e0a4db6623fea21a5d6df99cf55125
Joseph Hellerstein <jlheller@uw.edu>
2016-02-22 17:18:53 -0800

    Enhanced to pylint.sh to do all files in the directory.

--- bin/pylint.sh
+++ bin/pylint.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
-pylint --rcfile $HOME/BaseStack/bin/pylint.rcfile $@ | grep "," > /tmp/pylint.out
-grep -v "Unable to import" /tmp/pylint.out | more
+# Does a pylint of one or more files
+# Inputs: files to lint. If none, does all *.py files in directory
+OK="0"
+
+if [ "$#" -eq 0 ]
+then
+  FILES=`ls *.py`
+else
+  FILES=$@
+fi
+
+clear
+# Iterate across all the files to check
+for ff in $FILES; do
+  NO_LINES=`pylint --rcfile $HOME/BaseStack/bin/pylint.rcfile $ff \
+        | grep ":*,"  \
+        | grep -v 'Your code has been rated' \
+        | grep -v 'Unable to import' \
+        | wc | awk '{print $1}'`
+  if [ $NO_LINES -ne "0" ]
+  then
+    OK="1"
+    read -p "Press enter to see errors in $ff:"
+    echo ""
+    pylint --rcfile $HOME/BaseStack/bin/pylint.rcfile $ff \
+        | grep ","  \
+        | grep -v 'Your code has been rated' \
+        | grep -v 'Unable to import'
+  fi
+done
+
+if [ $OK -eq "0" ]
+then
+  echo "No errors found."
