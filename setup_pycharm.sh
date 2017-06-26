a0f78ea0d24374417cc971673d39ea9fea73f548
Joseph Hellerstein <jlheller@uw.edu>
2017-06-26 12:09:05 -0700

    Pycharm install script

---
+++ setup_pycharm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo add-apt-repository ppa:mystic-mirage/pycharm
+sudo apt-get update
+apt-get -f install
+#sudo apt-get install pycharm-community
+# Professional version
+sudo apt-get install pycharm
