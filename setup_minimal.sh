3af0348db8bf32ffb5361958adb63c9395e1547b
Joseph Hellerstein <jlheller@uw.edu>
2018-03-24 16:53:44 -0700

    Conda installs for both python2 and python3.

--- setup_minimal.sh
+++ setup_minimal.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 # Set-up for miniconda and installs
-rm -rf $HOME/miniconda3
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh
-rm Miniconda3-latest-Linux-x86_64.sh
-$HOME/miniconda3/bin/conda install numpy
-$HOME/miniconda3/bin/conda install pandas
-$HOME/miniconda3/bin/conda install jupyter-notebook
+function setup_conda {
+# Arg 1: python level either 2 or 3
+  PY=$1
+  rm -rf $HOME/miniconda${PY}
+  wget https://repo.continuum.io/miniconda/Miniconda${PY}-latest-Linux-x86_64.sh
+  bash Miniconda${PY}-latest-Linux-x86_64.sh
+  rm Miniconda${PY}-latest-Linux-x86_64.sh
+  $HOME/miniconda${PY}/bin/conda install numpy
+  $HOME/miniconda${PY}/bin/conda install pandas
+  $HOME/miniconda${PY}/bin/conda install jupyter notebook
+}
+
+#
+setup_conda 2
+setup_conda 3
 #
 sudo apt-get install vim
 cp vim/scripts.vim $HOME/.vimrc
+# Put shell scripts in path
+cp bin/.bashrc $HOME
+#
+Echo "Use conda2, conda3 for appropriate python versions."
