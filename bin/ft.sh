62e1a7cfed558579fd5c9943c53304e0aea9559c
Joseph Hellerstein <jlheller@uw.edu>
2018-07-24 12:17:36 -0700

    ft filters Binary files.

--- bin/ft.sh
+++ bin/ft.sh
@@ -2,4 +2,4 @@
 # Recursively finds the requested text in all files pattern.
 # Patterns should be quotes so that the shell doesn't resolve them to a file
 #echo $1
-grep -nr "$1" .
+grep -nr "$1" . | grep -v Binary
