db6ee0ff842ee0292cfa0b06d72d65f541b3c54a
Joe Hellerstein <jlheller@uw.edu>
2018-12-23 11:00:54 -0800

    Refined script to test python files.

--- bin/test_py.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Runs tests for all test_py files in the directory
-for f in $(ls test_*.py); do
-  echo ""
-  echo "***$f"
-  python $f
-done
---
+++ bin/testpy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Runs tests for all test_py files in the directory
+FILE="test_*.py"
+if [ ! -f ${FILE} ]; then
+    echo "No test_*.py file found!"
+    exit -1
+fi
+for f in ${FILE}; do
+  echo ""
+  echo "***$f"
+  python $f
+done
