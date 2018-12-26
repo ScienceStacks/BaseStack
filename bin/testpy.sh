e74b22c627730d281af91411d896deecb012e13f
Joe Hellerstein <jlheller@uw.edu>
2018-12-26 11:17:28 -0800

    Fix test for file present

--- bin/testpy.sh
+++ bin/testpy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Runs tests for all test_py files in the directory
-FILE="test_*.py"
+FILE=`ls | grep "test_.*.py"`
+COUNT=`echo ${FILE} | wc -w`
 #
-clear
-if [ ! -f ${FILE} ]; then
+#clear
+if [ $COUNT \= 0 ]; then
     echo "No test_*.py file found!"
     exit -1
 fi
