c8b37aa294d6a0f678784b8b84aed036a1d1cfa8
Joseph Hellerstein <jlheller@uw.edu>
2020-09-20 11:49:15 -0700

    Refine creating/running notebook scripts.

--- bin/run_nb.sh
+++ bin/run_nb.sh
@@ -1,17 +1,6 @@
-#!/bin/csh
+#!/bin/bash
 # Converts and executes an ipython notebook
 # Notes: (1) File should not contain spaces
-NOTEBOOK="$1.ipynb"
 PYTHON="$1.py"
-TMP1="/tmp/run_nb1.py"
-if test -f "${NOTEBOOK}"; then
-    echo "Creating ${PYTHON}$"
-else
-    echo "**Error. Cannot find ${NOTEBOOK}"
-    exit -1
-fi
-#
-jupyter nbconvert --to script "${NOTEBOOK}"
-sed 's/^# In\[.*$/print("&")/' < ${PYTHON}  >  ${TMP1}
-sed 's/^get_ipython().run/#&/' < ${TMP1}  > ${PYTHON}
+bash mk_nbscript.sh $1
 python ${PYTHON}
