f0510c3358b11ea576701e341a184886403f434e
Joseph Hellerstein <jlheller@uw.edu>
2022-07-24 18:03:46 -0700

    update

--- bin/activate.sh
+++ bin/activate.sh
@@ -4,3 +4,11 @@ if [ "x" = "x$1" ]; then
 else
     source $1/bin/activate
 fi
+# Setup the python path
+FILE="`pwd`/setup_run.sh"
+if [ -f "$FILE" ]; then
+    source $FILE
+else
+    PYTHONPATH=`pwd`
+    export $PYTHONPATH
+fi
