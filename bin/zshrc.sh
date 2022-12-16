e3285db1590856ada015bd124200b1b37d07c77d
joseph-hellerstein <joseph.hellerstein@gmail.com>
2022-12-16 08:44:02 -0800

    New zshrc

--- bin/zshrc.sh
+++ bin/zshrc.sh
@@ -1,4 +1,6 @@
-REPOPATH="/$HOME/repos"
+REPOPATH="/$HOME/home/Technical/repos"
 PS1='%~$ '
 PATH="${REPOPATH}/BaseStack/bin:/usr/local/bin::/usr/bin:/bin:/usr/sbin:/sbin:"
 source alias.sh
+# Enter a default python environment to protect the base environment
+source ~/home/Technical/repos/dft/bin/activate
