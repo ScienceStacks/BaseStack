fdb3fd975490d256ec7026771909891d60727f5f
Joseph Hellerstein <jlheller@uw.edu>
2026-06-06 20:34:08 -0700

    Updated setup_virtual

--- bin/setup_virtual.sh
+++ bin/setup_virtual.sh
@@ -4,18 +4,19 @@
 #   - in the directory where the environment is to be set up
 #   - requirements.txt exists
 # Arg 1 - name of virtual environment directory
+PYTHON_LEVEL="3.11"
 if [ "x" = "x$1" ]; then
     echo "***ERROR: Must specify the virtualization directory"
     exit -1
 fi
 DIR=$1
 #
-python3 -m venv ${DIR}
+virtualenv -p /opt/homebrew/bin/python${PYTHON_LEVEL} ${DIR}
 source ${DIR}/bin/activate
 pip install --upgrade pip
 # Check if need to install requirements
 if [ -f "requirements.txt" ]; then
-    pip install -r requirements.txt
+    pip install --no-cache-dir -r requirements.txt
 else 
     echo "***No requirements.txt found"
 fi
